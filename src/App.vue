76618ee5196757a4f33d193855b39d032931d911
Gaurav Kumar <gauravsng1994@gmail.com>
2020-06-06 02:06:30 +0530

    basic set up and certain components created

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" @del-todo="deleteTodo"/>
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data(){
+    return{
+      todos:[
+        { id:1, title:'Todo One', completed:false },
+        { id:2, title:'Todo Two', completed:true },
+        { id:3, title:'Todo Three', completed:false }
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !== id);
+      console.log("Hello",id);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
